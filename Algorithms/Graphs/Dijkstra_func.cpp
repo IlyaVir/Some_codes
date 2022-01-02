6ca8e6d6bcff8de737fd647fb62c2d6176413f12
IlyaVir <64567499+IlyaVir@users.noreply.github.com>
2022-01-02 20:02:52 +0300

    Update Dijkstra_func.cpp

--- Algorithms/Graphs/Dijkstra_func.cpp
+++ Algorithms/Graphs/Dijkstra_func.cpp
@@ -23,22 +23,15 @@ struct Vertex {
 
 vector <int> dijkstra(const vector <vector <Vertex>> &list_vertexes, int start = 0) {
     vector <int> distances(list_vertexes.size(), kINF);
-    // kINF - бесконечность, т.е. кратчайшего пути не найдено
     distances[start] = 0;
     set <pair <int, int>> next_vertexes;
-    // В next_vertexes хранятся в отсортированном виде пары
-    // {Дистанция, Номер_вершины_до_которой_не_нашли_минимальное_расстояние}.
-    // Таким образом первой в этой структуре будет ближайшая вершина, до
-    // которой не известно расстояние
     next_vertexes.insert({distances[start], start});
 
     while (!next_vertexes.empty()) {
         int from = next_vertexes.begin() -> second;
         next_vertexes.erase(next_vertexes.begin());
-        // Взяли ближайшую вершину
 
         for (const Vertex &to_vertex : list_vertexes[from]) {
-            // Обновляем расстояние с учётом взятой вершины
             if (distances[to_vertex.to()] > distances[from] + to_vertex.cost()) {
                 next_vertexes.erase({distances[to_vertex.to()], to_vertex.to()});
                 distances[to_vertex.to()] = distances[from] + to_vertex.cost();
