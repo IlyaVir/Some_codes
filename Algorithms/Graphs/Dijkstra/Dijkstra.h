73e9781827e63a5cac8b0e3da8610d3846842075
IlyaVir <64567499+IlyaVir@users.noreply.github.com>
2022-01-04 00:00:11 +0300

    Update Dijkstra.h

--- Algorithms/Graphs/Dijkstra/Dijkstra.h
+++ Algorithms/Graphs/Dijkstra/Dijkstra.h
@@ -3,27 +3,21 @@
 #include <vector>
 #include <set>
 #include <limits>
+#include <list>
 
 const size_t kINF = std::numeric_limits<size_t>::max();
 
 struct Vertex {
-    explicit Vertex(size_t to = 0, size_t cost = 0) : to_(to), cost_(cost) {}
-    size_t to_, cost_;
-
-    size_t to() const {
-        return to_;
-    }
-
-    size_t cost() const {
-        return cost_;
-    }
+    explicit Vertex(size_t to_ = 0, size_t cost_ = 0) : to(to_), cost(cost_) {}
+    
+    size_t to, cost;
 };
 
-std::vector <size_t> dijkstra(const std::vector <std::vector <Vertex>> &list_vertexes, size_t start = 0) {
-    // list_vertexes[from] = {to, cost}
+std::vector <size_t> dijkstra(const std::vector <std::list <Vertex>> &adjacency_list, size_t start = 0) {
+    // adjacency_list[from] = {to, cost}
     using namespace std;
 
-    vector <size_t> distances(list_vertexes.size(), kINF);
+    vector <size_t> distances(adjacency_list.size(), kINF);
     distances[start] = 0;
     set <pair <size_t, size_t>> next_vertexes;
     next_vertexes.insert({distances[start], start});
@@ -32,11 +26,11 @@ std::vector <size_t> dijkstra(const std::vector <std::vector <Vertex>> &list_ver
         size_t from = next_vertexes.begin() -> second;
         next_vertexes.erase(next_vertexes.begin());
 
-        for (const Vertex &to_vertex : list_vertexes[from]) {
-            if (distances[to_vertex.to()] > distances[from] + to_vertex.cost()) {
-                next_vertexes.erase({distances[to_vertex.to()], to_vertex.to()});
-                distances[to_vertex.to()] = distances[from] + to_vertex.cost();
-                next_vertexes.insert({distances[to_vertex.to()], to_vertex.to()});
+        for (const auto [to, cost] : adjacency_list[from]) {
+            if (distances[to] > distances[from] + cost) {
+                next_vertexes.erase({distances[to], to});
+                distances[to] = distances[from] + cost;
+                next_vertexes.insert({distances[to], to});
             }
         }
     }
