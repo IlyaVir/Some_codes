8cd1e1f5f03997a2e35ed2f479ccfd8ccb88e01d
IlyaVir <64567499+IlyaVir@users.noreply.github.com>
2022-01-03 17:42:20 +0300

    Update and rename Algorithms/Graphs/Floyd.cpp to Algorithms/Graphs/Floyd/Floyd.cpp

--- Algorithms/Graphs/Floyd/Floyd.cpp
+++ Algorithms/Graphs/Floyd/Floyd.cpp
@@ -1,13 +1,14 @@
 #include <vector>
 
 std::vector <std::vector <int>> floyd(std::vector <std::vector <int>> matrix) {
+    // matrix[i][j] < 0 => Пути/ребра не существует
     for (size_t middle = 0; middle < matrix.size(); ++middle) {
         for (size_t from = 0; from < matrix.size(); ++from) {
             for (size_t to = 0; to < matrix.size(); ++to) {
-                if (matrix[from][middle] >= 0 && matrix[middle][to] >= 0) { // возможный путь существуют
-                    if (matrix[from][to] >= 0) { // исходный путь существует
+                if (matrix[from][middle] >= 0 && matrix[middle][to] >= 0) {
+                    if (matrix[from][to] >= 0) {
                         matrix[from][to] = std::min(matrix[from][to], matrix[from][middle] + matrix[middle][to]);
-                    } else { // исходного пути не существует
+                    } else {
                         matrix[from][to] = matrix[from][middle] + matrix[middle][to];
                     }
                 }
